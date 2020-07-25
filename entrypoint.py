8451229575bfee5e50ca54b84c2e61d8cfa60bc6
Dipankar Pal <dipankarpal5050@gmail.com>
2020-07-25 11:20:57 +0530

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -3,65 +3,14 @@ import subprocess as sp
 import glob
 from PIL import Image
 
-# GITHUB_EVENT_NAME = os.environ['GITHUB_EVENT_NAME']
-
-# Set repository
-# CURRENT_REPOSITORY = os.environ['GITHUB_REPOSITORY']
-# TODO: How about PRs from forks?
-# TARGET_REPOSITORY = os.environ['INPUT_TARGET_REPOSITORY'] or CURRENT_REPOSITORY
-# PULL_REQUEST_REPOSITORY = os.environ['INPUT_PULL_REQUEST_REPOSITORY'] or TARGET_REPOSITORY
-# REPOSITORY = PULL_REQUEST_REPOSITORY if GITHUB_EVENT_NAME == 'pull_request' else TARGET_REPOSITORY
-
-# Set branches
-# GITHUB_REF = os.environ['GITHUB_REF']
-# GITHUB_HEAD_REF = os.environ['GITHUB_HEAD_REF']
-# GITHUB_BASE_REF = os.environ['GITHUB_BASE_REF']
-# CURRENT_BRANCH = GITHUB_HEAD_REF or GITHUB_REF.rsplit('/', 1)[-1]
-# TARGET_BRANCH = os.environ['INPUT_TARGET_BRANCH'] or CURRENT_BRANCH
-# PULL_REQUEST_BRANCH = os.environ['INPUT_PULL_REQUEST_BRANCH'] or GITHUB_BASE_REF
-# BRANCH = PULL_REQUEST_BRANCH if GITHUB_EVENT_NAME == 'pull_request' else TARGET_BRANCH
-
-# GITHUB_ACTOR = os.environ['GITHUB_ACTOR']
-# GITHUB_REPOSITORY_OWNER = os.environ['GITHUB_REPOSITORY_OWNER']
-
-
-#################
-GITHUB_TOKEN = os.environ['INPUT_GITHUB_TOKEN']
-
 
 MAX_HEIGHT_WIDTH = os.environ['INPUT_BASE_HEIGHT_WIDTH'] or "500"
-INPLACE = os.environ['INPUT_INPLACE'] or False
+INPLACE = os.environ['INPUT_INPLACE'] or "disable"
 
 ################################
 # INPLACE = True
-# MAX_HEIGHT_WIDTH =500
-def commit_changes():
-    """Commits changes.
-    """
-    set_email = 'git config --local user.email "custom-thumbnail@master"'
-    set_user = 'git config --local user.name "custom-thumbnail"'
-
-    sp.call(set_email, shell=True)
-    sp.call(set_user, shell=True)
-
-    git_checkout = f'git checkout {TARGET_BRANCH}'
-    git_add = f'git add .'
-    git_commit = 'git commit -m "Action: Images resized"'
-    print('Committing reports.......')
-
-    sp.call(git_checkout, shell=True)
-    sp.call(git_add, shell=True)
-    sp.call(git_commit, shell=True)
-
-
-def push_changes():
-    """Pushes commit.
-    """
-    set_url = f'git remote set-url origin https://x-access-token:{GITHUB_TOKEN}@github.com/{TARGET_REPOSITORY}'
-    git_push = f'git push origin {TARGET_BRANCH}'
-    sp.call(set_url, shell=True)
-    sp.call(git_push, shell=True)
-
+# MAX_HEIGHT_WIDTH = 500
+##############################
 
 def main():
 
@@ -91,12 +40,11 @@ def main():
         try:
             im = Image.open(entry)
             im.thumbnail(size,Image.ANTIALIAS)
-            if INPLACE==True:
+            if INPLACE=="enable":
                 im.save(entry)
                 print(f"wrote:  {entry} ----> {entry}")
             else:
                 out_path = os.path.abspath(f"./.thumbnails/{out_file}")
-                # out_path = os.path.abspath(os.path.join(os.getcwd(),"..",".thumb",out_file))
                 im.save(out_path)
                 print(f"wrote:  {entry} ----> {out_path}")
 
@@ -104,10 +52,6 @@ def main():
         except IOError:
             print(f" can not create thumbnail for {entry} Error: {IOError}")
 
-
-
-#     commit_changes()
-#     push_changes()
 
 
 if __name__ == '__main__':
